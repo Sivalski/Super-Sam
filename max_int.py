c906e41ad77c65a9f4118d377a537b6332420930
Sivalski <viktoro19@ru.is>
2019-09-05 10:01:57 +0000

    This is my commit

--- max_int.py
+++ max_int.py
@@ -1,2 +1,11 @@
-#Start by blablabla
-a = 0 
+num_int = int(input("Input a number: "))    # Do not change this line
+# Fill in the missing code
+max_int = 0
+num = 0
+while num >= 0:
+    #Max number, if number is higher then current max, we are going to store num as new max_num
+    if num_int > max_int:
+        max_int = num_int
+        num_int = int(input("Input a number: "))
+    print("The maximum is", max_int)    # Do not change this line
+else:
