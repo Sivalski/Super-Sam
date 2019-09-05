f83cedf00255751cced231c06124131865d0ede6
Sivalski <viktoro19@ru.is>
2019-09-05 10:26:59 +0000

    This is my commit

---
+++ sequence.py
@@ -0,0 +1,4 @@
+#1, 2, 3, 6, 11, 20, 37.... Pattern of numbers
+#Start with finding the correct pattern of numbers in sequence
+#Make an algorithm that continues with the pattern
+#Pattern is sum of 3 numbers, previous, middle and next.
